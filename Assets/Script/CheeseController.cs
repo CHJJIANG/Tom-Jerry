1f143d4e784c01c910466074fff9e02dcce074c8
bellinimoon <mg6839@nyu.edu>
2023-09-17 19:34:49 -0400

    UI

--- Assets/Script/CheeseController.cs
+++ Assets/Script/CheeseController.cs
@@ -7,6 +7,13 @@ public class CheeseController : MonoBehaviour
     public SpriteRenderer SP;
     public Sprite[] hurt;
 
+    public float[] xSize;
+    public float[] ySize;
+    public float[] xOffset;
+    public float[] yOffset;
+
+    BoxCollider2D BC;
+
     private int hurtTimes;
 
     // Start is called before the first frame update
@@ -14,14 +21,16 @@ public class CheeseController : MonoBehaviour
     {
         SP = gameObject.GetComponent<SpriteRenderer>();
         hurtTimes = 0;
+
+        BC = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
         SP.sprite = hurt[hurtTimes];
-
+        BC.size = new Vector2(xSize[hurtTimes], ySize[hurtTimes]);
+        BC.offset = new Vector2(xOffset[hurtTimes], yOffset[hurtTimes]);
     }
 
     void OnCollisionEnter2D(Collision2D collision)
@@ -29,7 +38,7 @@ public class CheeseController : MonoBehaviour
         
         if (collision.gameObject.CompareTag("Bullet"))
         {
-            if (hurtTimes < hurt.Length)
+            if (hurtTimes < hurt.Length - 1)
             {
                 hurtTimes++;
             }
