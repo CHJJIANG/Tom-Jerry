c7d536e84f82a03a13c8b7d1344effd133e18ad0
bellinimoon <mg6839@nyu.edu>
2023-09-17 13:37:23 -0400

    update

--- Assets/Script/Bullet.cs
+++ Assets/Script/Bullet.cs
@@ -1,5 +1,4 @@
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class Bullet : MonoBehaviour
 {
@@ -16,9 +15,18 @@ public class Bullet : MonoBehaviour
 
         if (collision.gameObject.CompareTag("player2"))
         {
+            
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        // 检查进入触发器的对象是否具有特定标签
+        if (other.CompareTag("Player"))
+        {
             // 在这里可以执行特定于玩家的操作
             // 获取玩家控制脚本
-            PlayerController playerController = collision.gameObject.GetComponent<PlayerController>();
+            PlayerController playerController = other.gameObject.GetComponent<PlayerController>();
 
             if (playerController != null)
             {
@@ -29,65 +37,15 @@ public class Bullet : MonoBehaviour
                 if (playerController.playerHealth <= 0)
                 {
                     // 玩家已死亡，可以执行相应的操作
-                    SceneManager.LoadScene(1);
                 }
             }
 
             // 销毁子弹对象
             Destroy(gameObject);
-            Debug.Log("life left: " + playerController.playerHealth);
-        }
+            Debug.Log("health: " + playerController.playerHealth);
 
-        if (collision.gameObject.CompareTag("Cheese2"))
-        {
-            // 在这里可以执行特定于玩家的操作
-            // 获取玩家控制脚本
-            PlayerController playerController = collision.gameObject.GetComponent<PlayerController>();
-
-            if (playerController != null)
-            {
-                // 减少芝士的生命值
-                playerController.playerHealth--;
-                // 加分
-                playerController.playerScore+=3;
-
-                // 处理玩家死亡逻辑
-                if (playerController.playerHealth <= 0)
-                {
-                    // 玩家已死亡，可以执行相应的操作
-                    SceneManager.LoadScene(1);
-                }
-            }
-
-            // 销毁子弹对象
-            Destroy(gameObject);
-            Debug.Log("Cheese Health: " + playerController.playerHealth);
-        }
-
-        if (collision.gameObject.CompareTag("Cheese1"))
-        {
-            // 在这里可以执行特定于玩家的操作
-            // 获取玩家控制脚本
-            PlayerController playerController = collision.gameObject.GetComponent<PlayerController>();
-
-            if (playerController != null)
-            {
-                // 减少芝士的生命值
-                playerController.playerHealth--;
-                // 加分
-                playerController.playerScore += 3;
-
-                // 处理玩家死亡逻辑
-                if (playerController.playerHealth <= 0)
-                {
-                    // 玩家已死亡，可以执行相应的操作
-                    SceneManager.LoadScene(1);
-                }
-            }
-
-            // 销毁子弹对象
-            Destroy(gameObject);
-            Debug.Log("Cheese Health: " + playerController.playerHealth);
         }
     }
 }
+
+
--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -1,18 +1,13 @@
 
 using UnityEngine;
-using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     public int playerHealth = 3; // 初始生命值
-    public int playerScore = 0; // 对手的分数
-
-    public Text PlayerScoreText;
-
 
     void Update()
     {
-        PlayerScoreText.text = playerScore.ToString();
+        // 在此可以添加玩家控制逻辑
     }
 
     // 其他玩家控制代码...
